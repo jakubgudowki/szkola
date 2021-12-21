a697e172c3fecd52f7d2b9f65ac40133ef2492f9
jakubgudowki <jakub.gudowski@o2.pl>
2021-12-21 19:03:24 +0100

    Add new files

---
+++ Zad2_pkt2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zad2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double silnia = 1;
+            for (int a = 1; a <= 20; a++)
+            {
+                silnia *= a;
+                Console.WriteLine(silnia);
+            }
+        }
+    }
+}
---
+++ Zad2_pkt3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zad2_3
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double silnia = 1;
+            for (int a = 1; a <= 243; a++)
+            {
+                silnia *= a;
+                Console.WriteLine(silnia);
+            }
+        }
+    }
+}
